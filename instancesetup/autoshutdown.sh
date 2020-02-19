5f77af18bc248093357793bb104f5b6e05bae475
Jacob Wisniewski <wisni116@cse.msu.edu>
2020-02-19 17:28:27 -0500

    Fixed autoshutdown to work with updated wording as of the 1.16 snapshots

--- instancesetup/autoshutdown.sh
+++ instancesetup/autoshutdown.sh
@@ -2,13 +2,14 @@
 SERVICE='server.jar'
 if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
     	PLAYERSEMPTY=" There are 0 of a max 20 players online"
+		PLAYERSEMPTY2=" There are 0 of a max of 20 players online"
 	$(screen -S minecraft -p 0 -X stuff "list^M")
 	sleep 5
 	$(screen -S minecraft -p 0 -X stuff "list^M")
 	sleep 5
 	PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
 	echo $PLAYERSLIST
-	if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]
+	if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ] || [ "$PLAYERSLIST" = "$PLAYERSEMPTY2" ]
 	then
 		echo "Waiting for players to come back in 12m, otherwise shutdown"
 		sleep 12m
@@ -17,7 +18,7 @@ if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
 		$(screen -S minecraft -p 0 -X stuff "list^M")
 		sleep 5
 		PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
-		if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]
+		if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ] || [ "$PLAYERSLIST" = "$PLAYERSEMPTY2" ]
 		then
 			$(sudo /sbin/shutdown -P +1)
 		fi

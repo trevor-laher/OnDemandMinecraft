4aef14cdf63733efc71c40b7a5cbcd05c5fa1c8e
potatoad <35428633+potatoad@users.noreply.github.com>
2021-12-24 17:17:09 +0000

    Update autoshutdown.sh
    Added " There are 0 out of maximum 20 players online." as new PLAYEREMPTY3 variable and removed colour formatting from PLAYERSLIST variable. This fixed issues with the script not shutting down my 1.18.1 Spigot server.

--- instancesetup/autoshutdown.sh
+++ instancesetup/autoshutdown.sh
@@ -2,14 +2,15 @@
 SERVICE='server.jar'
 if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
     	PLAYERSEMPTY=" There are 0 of a max 20 players online"
-		PLAYERSEMPTY2=" There are 0 of a max of 20 players online"
+	PLAYERSEMPTY2=" There are 0 of a max of 20 players online"
+	PLAYERSEMPTY3=" There are 0 out of maximum 20 players online."
 	$(screen -S minecraft -p 0 -X stuff "list^M")
 	sleep 5
 	$(screen -S minecraft -p 0 -X stuff "list^M")
 	sleep 5
-	PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
+	PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")
 	echo $PLAYERSLIST
-	if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ] || [ "$PLAYERSLIST" = "$PLAYERSEMPTY2" ]
+	if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ] || [ "$PLAYERSLIST" = "$PLAYERSEMPTY2" ] || [ "$PLAYERSLIST" = "$PLAYERSEMPTY3" ]
 	then
 		echo "Waiting for players to come back in 12m, otherwise shutdown"
 		sleep 12m
@@ -17,8 +18,8 @@ if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
 		sleep 5
 		$(screen -S minecraft -p 0 -X stuff "list^M")
 		sleep 5
-		PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
-		if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ] || [ "$PLAYERSLIST" = "$PLAYERSEMPTY2" ]
+		PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")
+		if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ] || [ "$PLAYERSLIST" = "$PLAYERSEMPTY2" ] || [ "$PLAYERSLIST" = "$PLAYERSEMPTY3" ]
 		then
 			$(sudo /sbin/shutdown -P +1)
 		fi
